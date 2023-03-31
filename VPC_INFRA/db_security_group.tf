9829f7fef1350e2d6bb6a1b93c9651edb5951a52
Hari.Thanneeru <harit.naidu@gmail.com>
2023-03-31 21:42:13 +0530

    updated DB SG

--- VPC_INFRA/db_security_group.tf
+++ VPC_INFRA/db_security_group.tf
@@ -3,6 +3,7 @@ resource "aws_security_group" "mysql" {
   description = "Security group for MySQL database"
   vpc_id      = aws_vpc.example.id
 
+
   ingress {
     from_port   = 3306
     to_port     = 3306
